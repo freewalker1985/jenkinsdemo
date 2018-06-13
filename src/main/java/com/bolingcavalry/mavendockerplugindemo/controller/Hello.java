77fcba785b3f1a9be1c36f3fc21d01335dfce996
freewalker1985 <freewalker1985@126.com>
2018-06-13 17:12:14 +0800

    jenkinsdemo
    fix

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "abcd. Hello jenkins, " + new Date();
+        return "test. Hello jenkins, " + new Date();
     }
 }
